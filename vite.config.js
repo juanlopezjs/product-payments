bb24af44ab6009bd648bced2ee76c74c31554b34
Juan Manuel López <juanma0474@hotmail.com>
2024-03-15 11:54:28 -0500

    feat(layout): add generic layout (#2)
    Layout is added for product detail pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import sass from 'sass';
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+      },
+    },
+  },
+  define: {
+    'process.env': process.env,
+  },
+  envPrefix: 'REACT_APP_',
+  plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,
+  },
 });
