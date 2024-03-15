638c47bb1b37fe337fba47beb84334712c678b84
Juan Manuel López <juanma0474@hotmail.com>
2024-03-14 20:25:20 -0500

    feat(project-struture): add project structure
    The structure of the project is defined with the hexagonal architecture

---
+++ src/shared/application/helpers/common-functions.js
@@ -0,0 +1,5 @@
+export const importFiles = async (importPaths) => {
+	const importPathArray = Object.values(importPaths).map((importPath) => importPath());
+	const result = await Promise.all(importPathArray.map((path) => path));
+	return result;
+};
---
+++ src/shared/application/helpers/history.js
@@ -0,0 +1,13 @@
+import { createBrowserHistory, createMemoryHistory } from "history";
+
+export const isServer = !(
+  typeof window !== "undefined" &&
+  window.document &&
+  window.document.createElement
+);
+
+export const history = isServer
+  ? createMemoryHistory({
+      initialEntries: ["/"],
+    })
+  : createBrowserHistory();
